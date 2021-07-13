0f4aa5e486503344848005120a09a095ac917cdd
json <json@momenti.tv>
2021-07-13 22:59:42 +0900

    Setup database with docker compose

--- config/dev.exs
+++ config/dev.exs
@@ -6,6 +6,7 @@ config :json_media, JsonMedia.Repo,
   password: "postgres",
   database: "json_media_dev",
   hostname: "localhost",
+  port: 45435,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
