6e20305cc6e7923a8ec65a5b36dacb6ab78d00b0
json <json@momenti.tv>
2021-08-28 23:07:43 +0900

    Change const to be evaluatated at runtime

--- apps/json_media/lib/json_media/common/const.ex
+++ apps/json_media/lib/json_media/common/const.ex
@@ -1,20 +1,22 @@
 defmodule JsonMedia.Const do
-  @const %{
-           posts_dir: Application.app_dir(:json_media, "priv/posts")
-         }
-         |> Map.merge(
-           case Mix.env() do
-             :test ->
-               %{
-                 posts_dir: "./test/support/fixture/posts"
-               }
-
-             _ ->
-               %{}
-           end
-         )
-
   def get() do
-    @const
+    %{
+      posts_dir: Application.app_dir(:json_media, "priv/posts")
+    }
+    |> Map.merge(
+      case env() do
+        :test ->
+          %{
+            posts_dir: "./test/support/fixture/posts"
+          }
+
+        _ ->
+          %{}
+      end
+    )
+  end
+
+  defp env() do
+    unquote(Mix.env())
   end
 end
